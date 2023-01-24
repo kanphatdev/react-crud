1e1f825ab29cfd3a6143035a3968290140b1b2af
kanphatratrazer <kanphatratrazer28@gmail.com>
2023-01-24 17:02:01 +0700

    add icon

--- src/component/User.js
+++ src/component/User.js
@@ -37,23 +37,23 @@ function User() {
   const handleDelete = id => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    
+
     var raw = JSON.stringify({
       "id": id
     });
-    
+
     var requestOptions = {
       method: 'DELETE',
       headers: myHeaders,
       body: raw,
       redirect: 'follow'
     };
-    
+
     fetch("https://www.melivecode.com/api/users/delete", requestOptions)
       .then(response => response.json())
       .then(result => {
         alert(result['message']);
-        if (result['status']==='ok') {
+        if (result['status'] === 'ok') {
           window.location.reload(false);
         }
       })
@@ -109,13 +109,19 @@ function User() {
                     <TableCell align="right">{row.username}</TableCell>
                     <TableCell align="right">
                       <ButtonGroup variant="outlined" aria-label="outlined button group">
-                        <Button onClick={()=> handleDelete(row.id)}>
-                          one
+                        <Button onClick={() => handleDelete(row.id)}>
+                          <span class="material-symbols-outlined">
+                            delete
+                          </span>
                         </Button>
                         <Link href='update' underline="none">
-                        <Button>Two</Button>
+                          <Button>
+                            <span class="material-symbols-outlined">
+                              edit
+                            </span>
+                          </Button>
                         </Link>
-                        
+
 
                       </ButtonGroup>
                     </TableCell>
